51da32e9f198ef03d23a437f06e6e5c5d37784a6
Vũ Ngọc Tú K14_FPL HN <tuvnph08581@fpt.edu.vn>
2020-05-16 00:37:22 +0700

    admin

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<!-- /.content-wrapper -->
+         <footer class="main-footer">
+            <strong>Copyright &copy; 2016-2017 <a href="#">Thememinister</a>.</strong> All rights reserved.
+         </footer>
+   
+      <!-- /.wrapper -->
