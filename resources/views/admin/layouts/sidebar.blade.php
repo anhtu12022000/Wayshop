ef44c26874c99ae7082f0c859d49f1664f82e06f
Vũ Ngọc Tú K14_FPL HN <tuvnph08581@fpt.edu.vn>
2020-05-21 11:45:14 +0700

    sa

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -30,29 +30,6 @@
                            </p>
                         </a>
                      </li>
-                     <li class="nav-item has-treeview">
-                        <a href="#" class="nav-link">
-                           <i class="nav-icon fas fa-table"></i>
-                           <p>
-                              Users
-                              <i class="fas fa-angle-left right"></i>
-                           </p>
-                        </a>
-                        <ul class="nav nav-treeview">
-                           <li class="nav-item">
-                              <a href="{{ url('admin/users') }}" class="nav-link">
-                                 <i class="far fa-circle nav-icon"></i>
-                                 <p>Add Users</p>
-                              </a>
-                           </li>
-                           <li class="nav-item">
-                              <a href="{{ url('admin/users') }}" class="nav-link">
-                                 <i class="far fa-circle nav-icon"></i>
-                                 <p>List Users</p>
-                              </a>
-                           </li>
-                        </ul>
-                     </li>
                      @role('Administrator')
                      <li class="nav-item has-treeview">
                         <a href="#" class="nav-link">
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+mix.js(['resources/js/wayshop/app.js',
+
+], 'public/front_assets/js/allscript.js')
